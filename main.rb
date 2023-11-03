8088ce71bdf11734bb3519e8e6122e050850af93
bobfox141 <bobfox141@gmail.com>
2023-11-03 16:42:38 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,23 @@
+require 'gosu'
+
+# class GameWindow inherits Gosu::Window
+class GameWindow < Gosu::Window
+  def initialize width=800, height=600, fullscreen=false
+    super
+    self.Caption = 'My Window' 
+    @message = Gosu::Image.from_text(self, "Hello Steve!!!", Gosu.default_font_name, 30)
+  end
+
+  def button_down
+    
+  end
+  
+  def draw
+    @message.draw(10,10,0)  
+  end
+end
+
+window = GameWindow.new
+window.show
+
+      
